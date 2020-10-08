5604c4bdea3e1865e08769467c95cb06758b4815
Lisa Gordon <lg.official94@gmail.com>
2020-10-08 15:04:40 -0500

    got the base page and form done!

--- src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -1,11 +1,53 @@
 package org.launchcode.skillstracker.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 //@RequestMapping("")
 public class SkillsController {
 
+    @GetMapping("")
+    @ResponseBody
+    public String baseText(){
+        return "<h1>Skills Tracker</h1>" +
+                "<h2>We have a few skills we would like to learn. Here is the list!</h2>" +
+                "<ol>" +
+                "<li>Java</li>" +
+                "<li>JavaScript</li>" +
+                "<li>Python</li>" +
+                "</ol>";
+    }
+
+    @GetMapping("form")
+    @ResponseBody
+    public String favLanguageForm() {
+        return "<html>" +
+                "<body>" +
+                "<form action='postrequestlang' method='post'>" +
+                "<p>Name:</p>" +
+                "<input type='text' name='name'><br>" +
+                "<p>My favorite language:</p><select name='language' id='language-select'>" +
+                "<option value='java'>Java</option>" +
+                "<option value='javascript'>JavaScript</option>" +
+                "<option value='python'>Python</option>" +
+                "</select><br>" +
+                "<p>My second favorite language:</p><select name='language' id='language-select'>" +
+                "<option value='java'>Java</option>" +
+                "<option value='javascript'>JavaScript</option>" +
+                "<option value='python'>Python</option>" +
+                "</select><br>" +
+                "<p>My third favorite language:</p><select name='language' id='language-select'>" +
+                "<option value='java'>Java</option>" +
+                "<option value='javascript'>JavaScript</option>" +
+                "<option value='python'>Python</option>" +
+                "</select><br>" +
+                "<input type='submit' value='Submit'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
 
 }
