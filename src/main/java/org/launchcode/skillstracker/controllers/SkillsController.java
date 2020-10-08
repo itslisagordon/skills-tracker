c521070c845b4d41c10e19b60d4d51e8cfabed18
Lisa Gordon <lg.official94@gmail.com>
2020-10-08 14:39:18 -0500

    set up chapter 10 studio repo

---
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -0,0 +1,11 @@
+package org.launchcode.skillstracker.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+//@RequestMapping("")
+public class SkillsController {
+
+
+}
